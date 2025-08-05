a6db91a1ee394d28c778b446d1bfedc3086e22c4
000Shehab000 <shhab200025@gmail.com>
2025-08-05 18:14:18 +0300

    Add React Router for navigation

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+function Home() {
+  return <h1>Home Page</h1>
+}
+
+function About() {
+  return <h1>About Page</h1>
+}
+
+function App() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
