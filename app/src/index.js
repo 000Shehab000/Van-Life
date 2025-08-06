48eac06471830689fc55d8974834af3a3f1fe25c
000Shehab000 <shhab200025@gmail.com>
2025-08-06 09:18:56 +0300

    Refactor and clean up imports

--- app/src/index.js
+++ app/src/index.js
@@ -1,21 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-
-function Home() {
-  return <h1>Home Page</h1>
-}
-
-function About() {
-  return <h1>About Page</h1>
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 
 function App() {
   return (
     <React.StrictMode>
       <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
