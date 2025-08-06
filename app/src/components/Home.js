fca046c405ea49ebe6eb4ab71a405ccccdd12e6a
000Shehab000 <shhab200025@gmail.com>
2025-08-06 10:22:25 +0300

    Add Home component and styles with background image

---
+++ app/src/components/Home.js
@@ -0,0 +1,22 @@
+import Header from './Header'
+import Footer from './Footer'
+import '../style/home.css'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="home-container">
+        <div className="discprition-container">
+          <h1>You got the travel plans, we got the travel vans.</h1>
+          <p>
+            Add adventure to your life by joining the #vanlife movement. Rent
+            the perfect van to make your perfect road trip.
+          </p>
+          <button>Find your van</button>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
