19c4a89a009b2c2ed5a6dea2dc9a5bedcb3072f6
000Shehab000 <shhab200025@gmail.com>
2025-08-07 09:58:11 +0300

    Fetch and display available vans in the Vans component

--- app/src/pages/Vans.js
+++ app/src/pages/Vans.js
@@ -1,20 +1,35 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import '../style/vans.css'
 import Van from '../components/VanCard'
+import '../style/vans.css'
+import { useState, useEffect } from 'react'
+import '../server'
 
 export default function Vans() {
+  const [vans, setVans] = useState([])
+
+  useEffect(() => {
+    fetch('/api/vans')
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans))
+  }, [])
+
+  const allVans = vans.map((vanObject) => (
+    <Van
+      key={vanObject.id}
+      name={vanObject.name}
+      imageUrl={vanObject.imageUrl}
+      type={vanObject.type}
+      price={vanObject.price}
+    />
+  ))
+
   return (
     <>
       <Header />
       <div className="vans-container">
         <h1>Explore our van options</h1>
-        <div className="vans-card-container">
-          <Van />
-          <Van />
-          <Van />
-          <Van />
-        </div>
+        <div className="vans-card-container">{allVans}</div>
       </div>
       <Footer />
     </>
