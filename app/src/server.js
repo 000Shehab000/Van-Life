621e193011c2b86ad5f19f82fdb96cd78ceb6678
000Shehab000 <shhab200025@gmail.com>
2025-08-09 19:25:10 +0300

    fix(server): use findBy instead of where
    - Corrected database query method.
    - Improved data retrieval efficiency.

--- app/src/server.js
+++ app/src/server.js
@@ -95,7 +95,7 @@ export default function makeServer() {
       this.get('/host/vans/:id', (schema, request) => {
         // Hard-code the hostId for now
         const id = request.params.id
-        return schema.vans.where({ id, hostId: '123' })
+        return schema.vans.findBy({ id, hostId: '123' })
       })
     },
   })
