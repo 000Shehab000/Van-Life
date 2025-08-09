b2e2dc9e693a2716da55b243b3578fc08e9c82c3
000Shehab000 <shhab200025@gmail.com>
2025-08-09 16:52:18 +0300

    feat(host): add HostVans page
    - Created HostVans component
    - Fetched van data from API
    - Displays host's vans with links

---
+++ app/src/pages/host/HostVans.js
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import HostVanCard from '../../components/HostVanCard'
+import '../../style/hostVans.css'
+import { useState, useEffect } from 'react'
+
+export default function HostVans() {
+  const [hostVans, setHostVans] = useState([])
+  useEffect(() => {
+    fetch('/api/host/vans')
+      .then((res) => res.json())
+      .then((data) => setHostVans(data.vans))
+  }, [])
+
+  const allHostVans = hostVans.map((hostVanobj) => (
+    <Link to={`/host/vans/${hostVanobj.id}`} key={hostVanobj.id}>
+      <HostVanCard
+        image={hostVanobj.imageUrl}
+        name={hostVanobj.name}
+        price={hostVanobj.price}
+      />
+    </Link>
+  ))
+
+  return (
+    <div className="host-vans-container">
+      <h1>Your listed vans</h1>
+      {allHostVans}
+    </div>
+  )
+}
