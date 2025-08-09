e967a21f36c79cd00f66fc2543cb2122486fcd3a
000Shehab000 <shhab200025@gmail.com>
2025-08-09 10:40:16 +0300

    Add HostLayout and nested routes for Dashboard, Income, and Reviews

--- app/src/index.js
+++ app/src/index.js
@@ -6,7 +6,11 @@ import About from './pages/About'
 import Vans from './pages/Vans'
 import VanDetails from './pages/VanDetails'
 import Layout from './Layout'
+import Dashboard from './pages/host/Dashboard'
+import Income from './pages/host/Income'
+import Reviews from './pages/host/Reviews'
 import makeServer from './server'
+import HostLayout from './components/HostLayout'
 
 makeServer()
 
@@ -15,11 +19,17 @@ function App() {
     <BrowserRouter>
       <React.StrictMode>
         <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/vans" element={<Vans />} />
-            <Route path="/vans/:id" element={<VanDetails />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="vans" element={<Vans />} />
+            <Route path="vans/:id" element={<VanDetails />} />
+
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Route>
         </Routes>
       </React.StrictMode>
