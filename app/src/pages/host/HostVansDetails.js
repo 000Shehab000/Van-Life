00a9b097d07a4b2c979d2e99d07f15390a0ce074
000Shehab000 <shhab200025@gmail.com>
2025-08-10 13:25:57 +0300

    feat(host): add HostVansDetails page
    - Created HostVansDetails component
    - Fetches van details via API
    - Displays van details and images
    - Includes navigation for details, pricing, photos
    - Uses react-router for navigation

---
+++ app/src/pages/host/HostVansDetails.js
@@ -0,0 +1,64 @@
+import { NavLink, Link, Outlet, useParams } from 'react-router-dom'
+import '../../style/header.css'
+import '../../style/hostVans.css'
+import { useEffect, useState } from 'react'
+
+export default function HostVansDetails() {
+  const { id } = useParams()
+
+  const [hostVanDetails, setHostVanDetails] = useState({})
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setHostVanDetails(data.vans))
+  }, [id])
+  console.log(hostVanDetails)
+
+  return (
+    <>
+      <nav className="back-button">
+        <Link to=".." relative="path">
+          &larr;Back to all vans
+        </Link>
+      </nav>
+      <div className="host-van-container">
+        <div className="host-van-details-container">
+          <img src={hostVanDetails.imageUrl} alt="" />
+          <div className="host-van-details-text">
+            <div className={`category ${hostVanDetails.type}`}>
+              {hostVanDetails.type}
+            </div>
+            <h2>{hostVanDetails.name}</h2>
+            <span className="price">
+              <b>${hostVanDetails.price} </b>/day
+            </span>
+          </div>
+        </div>
+        <nav>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to={`/host/vans/${id}`}
+            end
+          >
+            Details
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to={`/host/vans/${id}/pricing`}
+          >
+            Pricing
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to={`/host/vans/${id}/photos`}
+          >
+            Photos
+          </NavLink>
+        </nav>
+
+        <Outlet />
+      </div>
+    </>
+  )
+}
