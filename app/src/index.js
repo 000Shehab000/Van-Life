df9edd81f02d37d3e31d1a5c6c4093d291c4c1d4
000Shehab000 <shhab200025@gmail.com>
2025-08-17 15:47:53 +0300

    refactor(app): remove unnecessary StrictMode
    - Removed `React.StrictMode` wrapper.

--- app/src/index.js
+++ app/src/index.js
@@ -9,7 +9,7 @@ import Layout from './Layout'
 import Dashboard from './pages/host/Dashboard'
 import Income from './pages/host/Income'
 import Reviews from './pages/host/Reviews'
-import makeServer from './server'
+import './server'
 import HostLayout from './components/HostLayout'
 import HostVans from './pages/host/HostVans'
 import HostVansDetails from './pages/host/HostVansDetails'
@@ -18,34 +18,30 @@ import Pricing from './pages/host/Pricing'
 import Photos from './pages/host/Photos'
 import NotFound from './pages/NotFound'
 
-makeServer()
-
 function App() {
   return (
     <BrowserRouter>
-      <React.StrictMode>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="vans" element={<Vans />} />
-            <Route path="vans/:id" element={<VanDetails />} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetails />} />
 
-            <Route path="host" element={<HostLayout />}>
-              <Route index element={<Dashboard />} />
-              <Route path="income" element={<Income />} />
-              <Route path="reviews" element={<Reviews />} />
-              <Route path="vans" element={<HostVans />} />
-              <Route path="vans/:id" element={<HostVansDetails />}>
-                <Route index element={<Details />} />
-                <Route path="pricing" element={<Pricing />} />
-                <Route path="photos" element={<Photos />} />
-              </Route>
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVansDetails />}>
+              <Route index element={<Details />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="photos" element={<Photos />} />
             </Route>
-            <Route path="*" element={<NotFound />} />
           </Route>
-        </Routes>
-      </React.StrictMode>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
