9aafd3cf776818fd1082cac850ffc88c8b4dbda4
000Shehab000 <shhab200025@gmail.com>
2025-09-10 19:10:01 +0300

    refactor(Vans.js): remove unused useEffect hook

--- app/src/pages/Vans.js
+++ app/src/pages/Vans.js
@@ -1,6 +1,6 @@
 import Van from '../components/VanCard'
 import '../style/vans.css'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Link, useLoaderData, useSearchParams } from 'react-router-dom'
 import { getVans } from '../api'
 
