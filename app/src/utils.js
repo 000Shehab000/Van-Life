907f234b247cfb9357a2542e379688168f108517
000Shehab000 <shhab200025@gmail.com>
2025-09-13 16:12:51 +0300

    feat(auth): add authentication requirement
    - Created requireAuth utility function
    - Redirects unauthenticated users to /login

---
+++ app/src/utils.js
@@ -0,0 +1,22 @@
+import { redirect } from 'react-router-dom'
+
+export async function requireAuth() {
+  const isLoggedIn = false
+
+  const response = redirect('/login')
+  response.body = true // It's silly, but it works using mirage JS
+
+  if (!isLoggedIn) {
+    return response
+  }
+}
+
+// it should work while not using mirage JS
+
+// export async function requireAuth() {
+//   const isLoggedIn = false
+
+//   if (!isLoggedIn) {
+//     throw redirect('/login')
+//   }
+// }
